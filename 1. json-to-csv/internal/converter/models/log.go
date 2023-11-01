fea71655bbbaac67b192afa1d900e3ccf5246de4
Георгий Кораблёв <jasmanchik@gmail.com>
2023-11-01 16:26:25 +0500

    refactor

--- 1. json-to-csv/internal/converter/models/log.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type LogRow struct {
-	Lat  float64                `json:"latitude"`
-	Lon  float64                `json:"longitude"`
-	Id   string                 `json:"id"`
-	Tags map[string]interface{} `json:"tags"`
-}
